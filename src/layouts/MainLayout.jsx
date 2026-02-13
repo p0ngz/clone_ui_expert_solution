3dcb866abd908e9805ad196c03731970926a0b5a
p0ngz <naepongsatorn@gmail.com>
2026-02-13 19:51:18 +0700

    set up initial project clone_ui_expert_solution

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,7 @@
 import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
-    <div className="w-screen min-h-screen bg-gray-500 text-white">
-      <h1>This is MainLayout</h1>
+    <div className="w-screen min-h-screen">
       <Outlet />
     </div>
   );
